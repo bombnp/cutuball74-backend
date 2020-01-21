f8c325bb5eb32aa2e7f8d18352b79186c4f4f2af
bombnp <bomb.NP@gmail.com>
2020-01-21 14:28:13 +0700

    Added TCP connection

--- api/config.js
+++ api/config.js
@@ -1,8 +1,17 @@
 const dbConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  socketPath: process.env.DB_SOCKET
+  "TCP": {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
+  },
+  "UNIX": {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    socketPath: process.env.DB_SOCKET
+  }
 };
 
 const secret = process.env.SERVER_SECRET;
--- api/model/database.js
+++ api/model/database.js
@@ -4,7 +4,7 @@ const config = require('../config.js');
 
 let pool;
 
-mysql.createPool(config.dbConfig).then(
+mysql.createPool(config.dbConfig[process.env.DB_CONNECTION]).then(
   (res) => {
     pool = res;
   }
