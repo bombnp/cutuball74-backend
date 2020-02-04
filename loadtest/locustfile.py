247849ffb39893b75ec5c5b9a9995df93d32c8e6
Pipat Saengow <pay2630@gmail.com>
2020-02-04 20:48:27 +0700

    Load testing checkin API

--- loadtest/locustfile.py
+++ loadtest/locustfile.py
@@ -29,32 +29,63 @@ class UserTask(TaskSet):
         "name": genrandstr(thaicharset, 20) + ' ' + genrandstr(thaicharset, 5) + ' ' + genrandstr(thaicharset + ascii_letters, 20),
         "email": genrandstr(charlen = 40) + '@' + genrandstr(charlen = 10) + '.com',
         "faculty": "21",
-        "tel": '0' + genrandstr(charset = '0123456789', charlen= 9)
+        "tel": '0' + genrandstr(charset = '0123456789', charlen= 9),
         }
         self.register()
         self.login()
+        self.stafflogin()
+        self.checkin = False
 
 
     def register(self):
-        self.client.post(json = self.ud,
+        modud = dict(self.ud)
+        modud["bypassrecaptcha"] = "true"
+        res = self.client.post(json = modud,
             url = "/register")
+
+
+    def reqtoken(self, username, password):
+        res = self.client.post("/token", data = {
+            "grant_type" : "password",
+            "username" : username,
+            "password" : password
+            })
+        return res.json()['access_token']
 
 
     @task(5)
     def login(self):
-        res = self.client.post("/token", data = {
-            "grant_type" : "password",
-            "username" : self.ud['ID'],
-            "password" : self.ud['tel']
-            })
+        token = self.reqtoken(self.ud['ID'], self.ud['tel'])
+        self.headers = {'Authorization': 'Bearer ' + token}
 
-        self.headers = {'Authorization': 'Bearer ' + res.json()['access_token']}
 
+    def stafflogin(self):
+        self.staffheaders = {'Authorization': 'Bearer ' + self.reqtoken("staff", "IaMsTaFf")}
 
     @task(20)
     def getuser(self):
-        res = self.client.get('/getuser', headers = self.headers).json()
-        assert(res == self.ud)
+        with self.client.get('/getuser', headers = self.headers, catch_response=True) as res:
+            if res.status_code == 200 and res.json() == self.ud:
+                res.success()
+            else:
+                res.failure("getuser status err or data mismatch")
+                print(str(self.ud) + " <> " + str(res.json()))
+
+    @task(5)
+    def checkin(self):
+        with self.client.post('/staff/checkin', headers = self.staffheaders, json = {"id":self.ud['ID']}, catch_response=True) as res:
+            if not self.checkin:
+                if res.status_code == 200:
+                    res.success()
+                    self.checkin = True
+                else:
+                    res.failure("Checking fail")
+                
+            else:
+                if res.status_code == 409:
+                    res.success()
+                else:
+                    res.failure("Re-checkin fail")
 
 
 class WebsiteUser(HttpLocust):
